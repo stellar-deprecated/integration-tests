3f4ab7565f90b59eeaf3292309ddc05c007a5aeb
Bartek Nowotarski <bartek@nowotarski.info>
2018-06-29 15:33:35 +0200

    Fix dependencies.sh

--- container/dependencies.sh
+++ container/dependencies.sh
@@ -1,13 +1,14 @@
 set -e
 
 apt-get update
-apt-get install -y git gcc postgresql openssl mysql-client
+apt-get install -y git mercurial gcc curl postgresql openssl
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
-wget -nv https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
-tar xf go1.8.linux-amd64.tar.gz
+wget -nv https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
+tar xf go1.10.linux-amd64.tar.gz
 mv go /usr/local
 
-mkdir /.gopath
-go get -v github.com/constabulary/gb/...
+mkdir -p /.gopath/bin
+mkdir -p /.gopath/src
+curl https://glide.sh/get | sh
