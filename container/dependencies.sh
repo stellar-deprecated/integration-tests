aaed936e0746c0368b6c21a96fd12188694e534d
Leigh McCulloch <leigh@mcchouse.com>
2019-09-04 14:24:50 -0700

    Update Go to 1.13

--- container/dependencies.sh
+++ container/dependencies.sh
@@ -5,8 +5,8 @@ apt-get install -y git mercurial gcc curl postgresql openssl
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
-wget -nv https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
-tar xf go1.10.linux-amd64.tar.gz
+wget -nv https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+tar xf go1.13.linux-amd64.tar.gz
 mv go /usr/local
 
 mkdir -p /.gopath/bin
