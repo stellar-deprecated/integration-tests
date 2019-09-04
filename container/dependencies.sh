3dad3043ebb6d2aaa87b571195f194e5dbf26089
Leigh McCulloch <leigh@stellar.org>
2019-09-04 16:46:56 -0700

    Remove use of dep and update version of Go to Go 1.13 (#8)
    Remove use of `dep` and update version of Go to Go 1.13.
    
    The `stellar/go` repo was changed in stellar/go#1542 to use the build-in Go Modules instead of Dep for dependency management. The standard `go` commands can now be relied on for downloading and installing dependencies without any need to run any additional commands like we had to with Dep. Go 1.13 is the latest version of Go and we should use it for these tests since the repository no longer supports Go 1.10.

--- container/dependencies.sh
+++ container/dependencies.sh
@@ -5,10 +5,9 @@ apt-get install -y git mercurial gcc curl postgresql openssl
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
-wget -nv https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
-tar xf go1.10.linux-amd64.tar.gz
+wget -nv https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+tar xf go1.13.linux-amd64.tar.gz
 mv go /usr/local
 
 mkdir -p /.gopath/bin
 mkdir -p /.gopath/src
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
